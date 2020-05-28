fbaeda7843f8123794fd4ca29c05f75f114bd98f
Bryan Triana <BryanTrianaRueda@gmail.com>
2020-05-28 09:44:05 -0400

    Add functions to load selectors and highlight active page

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,28 +1,30 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/**
+ * Loads a selector from another HTML file so that it can be used in
+ * the current DOM
+ *
+ * @param { string } selectorID - ID that will be used across the document to refer to the HTML element
+ * @param { string } filename - Filename of HTML file that is used to load the element
+ *
+ * @example loadSelector("navbar", "navbar.html")
+ */
+function loadSelector(selectorID, filename) {
+    $(document).ready(function() {
+        $('#' + selectorID).load(filename);
+    });
+}
 
 /**
- * Adds a random greeting to the page.
+ * Highlights the current active link on the navigation bar. The links used
+ * to navigate the website should be enclosed by an element with ID 'link-container'.
+ * This function should be called after the navigation bar is defined in the DOM tree
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+function highlightActivePage() {
+    const linkContainer = document.getElementById('link-container');
+    const links = linkContainer.getElementsByTagName('a');
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+    for(let i = 0; i < links.length; ++i) {
+        if(links[i].href === window.location.href) {
+            links[i].classList.add('active');
+        }
+    }
 }
