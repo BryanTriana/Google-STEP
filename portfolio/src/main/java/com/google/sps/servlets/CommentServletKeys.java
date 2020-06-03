26654c14c737b67cfd5138e0170eb0463d30f3eb
Bryan Triana <BryanTrianaRueda@gmail.com>
2020-06-03 16:22:13 +0000

    Make class package private and add private constructor
    Adding a private constructor will make sure that nobody will be able to create objects of this class, making the intent of the usage of this class clear

--- portfolio/src/main/java/com/google/sps/servlets/CommentServletKeys.java
+++ portfolio/src/main/java/com/google/sps/servlets/CommentServletKeys.java
@@ -3,10 +3,12 @@ package com.google.sps.servlets;
 /**
  * Holds the values for the keys to access elements in the Datastore Service.
  */
-public class CommentServletKeys {
-  public static final String COMMENT_KIND = "Comment";
-  public static final String NAME_PROPERTY = "name";
-  public static final String MESSAGE_PROPERTY = "message";
-  public static final String TIMESTAMP_PROPERTY = "timestamp";
-  public static final String COMMENT_LIMIT_PROPERTY = "commentLimit";
+class CommentServletKeys {
+  static final String COMMENT_KIND = "Comment";
+  static final String NAME_PROPERTY = "name";
+  static final String MESSAGE_PROPERTY = "message";
+  static final String TIMESTAMP_PROPERTY = "timestamp";
+  static final String COMMENT_LIMIT_PROPERTY = "commentLimit";
+
+  private CommentServletKeys() {}
 }
