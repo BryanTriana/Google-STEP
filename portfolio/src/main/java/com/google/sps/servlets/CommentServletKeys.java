c42e40659782c3ed85338dafea67c62a9fbfba51
Bryan Triana <BryanTrianaRueda@gmail.com>
2020-06-03 13:45:54 +0000

    Add comment limit parameter to fetch a specific number of comments

---
+++ portfolio/src/main/java/com/google/sps/servlets/CommentServletKeys.java
@@ -0,0 +1,12 @@
+package com.google.sps.servlets;
+
+/**
+ * Holds the values for the keys to access elements in the Datastore Service.
+ */
+public class CommentServletKeys {
+  public static final String COMMENT_KIND = "Comment";
+  public static final String NAME_PROPERTY = "name";
+  public static final String MESSAGE_PROPERTY = "message";
+  public static final String TIMESTAMP_PROPERTY = "timestamp";
+  public static final String COMMENT_LIMIT_PROPERTY = "commentLimit";
+}
