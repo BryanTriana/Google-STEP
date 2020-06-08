4ffd3fb777cac06e6a2644496fd8b9d2bf8111df
Bryan Triana <BryanTrianaRueda@gmail.com>
2020-06-08 19:54:09 +0000

    Implement nickname feature to avoid displaying an email when the user posts a comment

---
+++ portfolio/src/main/java/com/google/sps/servlets/NicknameFinder.java
@@ -0,0 +1,41 @@
+package com.google.sps.servlets;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
+
+/**
+ * Utility class used to find the nickname of a given User Entity.
+ */
+final class NicknameFinder {
+  /**
+   * Searches for the nickname of a User Entity with a given ID.
+   *
+   * @param id The ID property associated with the entity whose nickname we want to find
+   * @return The nickname of the entity with the given id or an empty string if not found
+   */
+  static String getNickname(String id) {
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+    Query query =
+        new Query(UserKeys.USER_KIND)
+            .setFilter(new FilterPredicate(UserKeys.ID_PROPERTY, FilterOperator.EQUAL, id));
+    PreparedQuery queryResults = datastore.prepare(query);
+
+    Entity userEntity = queryResults.asSingleEntity();
+
+    if (userEntity == null) {
+      return "";
+    }
+
+    String nickname = (String) userEntity.getProperty(UserKeys.NICKNAME_PROPERTY);
+
+    return nickname;
+  }
+
+  private NicknameFinder(){};
+}
---
+++ portfolio/src/main/java/com/google/sps/servlets/NicknameServlet.java
@@ -0,0 +1,46 @@
+package com.google.sps.servlets;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.gson.Gson;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Stores and fetches user nicknames.
+ */
+@WebServlet("/nickname-data")
+public class NicknameServlet extends HttpServlet {
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    UserService userService = UserServiceFactory.getUserService();
+
+    String nickname = NicknameFinder.getNickname(userService.getCurrentUser().getUserId());
+
+    response.setContentType("application/json");
+    response.getWriter().println(new Gson().toJson(nickname));
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    UserService userService = UserServiceFactory.getUserService();
+
+    String id = userService.getCurrentUser().getUserId();
+    String nickname = request.getParameter(UserKeys.NICKNAME_PROPERTY);
+
+    Entity userDataEntity = new Entity(UserKeys.USER_KIND, id);
+    userDataEntity.setProperty(UserKeys.ID_PROPERTY, id);
+    userDataEntity.setProperty(UserKeys.NICKNAME_PROPERTY, nickname);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(userDataEntity);
+
+    response.sendRedirect("/blog.html");
+  }
+}
